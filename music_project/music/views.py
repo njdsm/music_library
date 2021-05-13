5dfb1512ec1e7a83a1f891bf39fc7a0d4ec84608
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-05-13 14:17:39 -0500

    completed put and delete views for songs

--- music_project/music/views.py
+++ music_project/music/views.py
@@ -35,4 +35,15 @@ class SongDetail(APIView):
         serializer = SongSerializer(song)
         return Response(serializer.data)
 
-    
+    def put(self, request, pk):
+        song = self.get_object(pk)
+        serializer = SongSerializer(song, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        song = self.get_object(pk)
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
