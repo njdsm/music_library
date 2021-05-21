5336d0f3a1503ad18e58103661bfd59e2c7398ad
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-05-21 15:38:04 -0500

    modified release_date to be just datefield instead of datetimefield.

--- music_project/music/models.py
+++ music_project/music/models.py
@@ -7,6 +7,6 @@ class Song(models.Model):
     title = models.CharField(max_length=50, blank=True, null=True)
     artist = models.CharField(max_length=50, blank=True, null=True)
     album = models.CharField(max_length=50, blank=True, null=True)
-    release_date = models.DateTimeField(blank=True, null=True)
+    release_date = models.DateField(blank=True, null=True)
     likes = models.IntegerField(blank=True, null=True)
     genre = models.CharField(max_length=50, blank=True, null=True)
